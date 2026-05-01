12722723b2fd2c320fa782078c481b62d53077b8
sounth-coder <sountharikan.thiru@gmail.com>
2026-05-01 15:59:48 +1000

    Modified routing and new sections under index

--- app.py
+++ app.py
@@ -86,6 +86,11 @@ def index():
     """Landing page"""
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
